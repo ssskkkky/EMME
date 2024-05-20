0af5f1b5a7cca196bfc91e3071c3c816e66e2767
qzhong <12ff54e@gmail.com>
2024-05-20 11:19:36 +0800

    Change integrate from recursive to iterative

--- functions.h
+++ functions.h
@@ -7,6 +7,7 @@
 #include <limits>
 #include <string>
 #include <type_traits>
+#include <vector>
 
 // Function to read contents of a file line by line
 std::string readInputFromFile(const std::string& filename);
@@ -172,32 +173,36 @@ struct gauss_kronrod : gauss_kronrod_detail<N> {
                                        Tx local_abs_tol,
                                        Tx global_rel_tol)
         -> decltype(std::declval<Func>()(std::declval<Tx>())) {
-        const Tx mid = (b + a) / 2;
-        const Tx scale = (b - a) / 2;
-        auto normalize_func = [&](Tx x) { return func(scale * x + mid); };
+        // call stack
+        std::vector<std::array<Tx, 3>> pending_intervals;
+        // quadrature sum
+        decltype(std::declval<Func>()(std::declval<Tx>())) sum{};
+        pending_intervals.push_back({a, b, Tx{}});
+        while (!pending_intervals.empty()) {
+            auto [l, r, local_abs_tol] = pending_intervals.back();
+            pending_intervals.pop_back();
 
-        auto result = gauss_kronrod_basic(normalize_func);
-        auto integral = result.first * scale;
-        auto err = result.second * scale;
-        if (std::fpclassify(local_abs_tol) == FP_ZERO) {
-            local_abs_tol = std::abs(global_rel_tol * integral);
+            const Tx mid = (r + l) / 2;
+            const Tx scale = (r - l) / 2;
+            auto normalize_func = [&](Tx x) { return func(scale * x + mid); };
+            auto result = gauss_kronrod_basic(normalize_func);
+            auto integral = result.first * scale;
+            auto err = result.second * scale;
+
+            if (std::fpclassify(local_abs_tol) == FP_ZERO) {
+                local_abs_tol = std::abs(global_rel_tol * integral);
+            }
+            if (std::ldexp(scale, max_subdivide) > 0.99 * (b - a) &&
+                err > local_abs_tol &&
+                err > std::abs(global_rel_tol * integral)) {
+                pending_intervals.push_back({mid, r, local_abs_tol / 2});
+                pending_intervals.push_back({l, mid, local_abs_tol / 2});
+            } else {
+                sum += integral;
+            }
         }
 
-        if (max_subdivide > 0 && err > local_abs_tol &&
-            err > std::abs(global_rel_tol * integral)) {
-            return gauss_kronrod_adaptive(func, a, mid, max_subdivide - 1,
-                                          local_abs_tol / 2, global_rel_tol) +
-                   gauss_kronrod_adaptive(func, mid, b, max_subdivide - 1,
-                                          local_abs_tol / 2, global_rel_tol);
-        }
-
-#ifdef _TRACE
-        std::cout << "[TRACE] Gauss-Kronrod subdivide stopped at [" << a << ", "
-                  << b << "] with remaining subdivision = " << max_subdivide
-                  << ", err = " << err << '\n';
-#endif
-
-        return integral;
+        return sum;
     }
 };
 
